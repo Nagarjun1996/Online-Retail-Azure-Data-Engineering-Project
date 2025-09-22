409d5021f412280df02719363ae732ea6d663f3b
Nagarjun1996 <nagarjuna.learner96@gmail.com>
2025-09-22 02:28:12 -0500

    Delete azure-online-retail-lakehouse/databricks/00_config.py

--- azure-online-retail-lakehouse/databricks/00_config.py
+++
@@ -1,4 +0,0 @@
-BRONZE_PATH = "abfss://<container>@<account>.dfs.core.windows.net/bronze/online_retail/"
-SILVER_PATH = "abfss://<container>@<account>.dfs.core.windows.net/silver/online_retail/"
-GOLD_PATH   = "abfss://<container>@<account>.dfs.core.windows.net/gold/online_retail/"
-PARTITION_COLS = ["year", "month"]
