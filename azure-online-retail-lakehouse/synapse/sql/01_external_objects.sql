e7ba5d4726f6df9eec5d8742c2896aa2f0d8d2d6
Nagarjun1996 <nagarjuna.learner96@gmail.com>
2025-09-22 02:38:00 -0500

    Update 01_external_objects.sql

--- azure-online-retail-lakehouse/synapse/sql/01_external_objects.sql
+++ azure-online-retail-lakehouse/synapse/sql/01_external_objects.sql
@@ -1,6 +1,39 @@
+---------------------
+-- CREATE CREDENTIAL 
+---------------------
+
+CREATE DATABASE SCOPED CREDENTIAL arjuncred
+WITH
+    IDENTITY = 'Managed Identity'
+
+-------------------------------
+-- CREATE EXTERNAL DATA SOURCE 
+-------------------------------
 CREATE EXTERNAL DATA SOURCE OnlineRetail
-WITH (
-    LOCATION = 'abfss://<container>@<account>.dfs.core.windows.net',
-    CREDENTIAL = (IDENTITY = 'Managed Identity')
-);
-CREATE EXTERNAL FILE FORMAT delta_format WITH (FORMAT_TYPE = DELTA);
+WITH
+(
+    LOCATION = 'https://onlineretail96.dfs.core.windows.net/gold' ,
+    CREDENTIAL = arjuncred
+)
+
+------------------
+-- OPEN ROW SET 
+------------------
+
+SELECT 
+*
+FROM
+    OPENROWSET(
+            BULK 'Data_warehouse',
+            DATA_SOURCE = 'OnlineRetail',
+            FORMAT = 'DELTA'
+    ) AS query1
+
+-------------------------------
+-- CREATE EXTERNAL FILE FORMAT 
+-------------------------------
+
+CREATE EXTERNAL FILE FORMAT delta_format
+WITH(
+        FORMAT_TYPE = DELTA 
+)
