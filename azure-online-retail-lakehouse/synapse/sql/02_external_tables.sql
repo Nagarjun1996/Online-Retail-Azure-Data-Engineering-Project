9cfa8257090fb12c8734dc1a1a6c8d7d32d246ed
Nagarjun1996 <nagarjuna.learner96@gmail.com>
2025-09-22 02:38:48 -0500

    Update 02_external_tables.sql

--- azure-online-retail-lakehouse/synapse/sql/02_external_tables.sql
+++ azure-online-retail-lakehouse/synapse/sql/02_external_tables.sql
@@ -1,37 +1,23 @@
-IF OBJECT_ID('dbo.OnlineRetail_dw', 'U') IS NOT NULL
-    DROP EXTERNAL TABLE dbo.OnlineRetail_dw;
-CREATE EXTERNAL TABLE dbo.OnlineRetail_dw
+CREATE EXTERNAL TABLE OnlineRetail_dw
 (
-    Invoice       NVARCHAR(50),
-    StockCode     NVARCHAR(50),
-    Description   NVARCHAR(2000),
-    Quantity      INT,
-    InvoiceDate   DATETIME2,
-    Price         FLOAT,
-    CustomerID    NVARCHAR(400),
-    Country       NVARCHAR(100),
-    Year          INT,
-    Month         INT,
-    Revenue       FLOAT
+    Invoice NVARCHAR(50),
+    StockCode NVARCHAR(50),
+    Description NVARCHAR(2000),
+    Quantity INT,
+    InvoiceDate DATETIME2,
+    Price FLOAT,
+    CustomerID VARCHAR(400),
+    Country NVARCHAR(100),
+    Year INT,
+    Month INT,
+    Revenue FLOAT
 )
 WITH
 (
-    LOCATION = 'gold/online_retail/orders_silver',
+    LOCATION = 'Data_warehouse',
     DATA_SOURCE = OnlineRetail,
     FILE_FORMAT = delta_format
 );
 
-IF OBJECT_ID('dbo.OnlineRetail_RevenueByCountry', 'U') IS NOT NULL
-    DROP EXTERNAL TABLE dbo.OnlineRetail_RevenueByCountry;
-CREATE EXTERNAL TABLE dbo.OnlineRetail_RevenueByCountry
-(
-    Country NVARCHAR(100),
-    TotalRevenue FLOAT,
-    NumRows BIGINT
-)
-WITH
-(
-    LOCATION = 'gold/online_retail/mart_revenue_by_country',
-    DATA_SOURCE = OnlineRetail,
-    FILE_FORMAT = delta_format
-);
+
+SELECT * FROM OnlineRetail_dw
